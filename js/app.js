bd81ca5462df2415cfcc216a4f0cb56e0989d17d
aberkow <berkowitz.clarinet@gmail.com>
2016-03-07 22:04:08 -0500

    finally got a basic JSON request working for youtube.

--- js/app.js
+++ js/app.js
@@ -10,16 +10,52 @@ Bonus
 */
 
 $(document).ready(function(){
-  $.getJSON('https://www.googleapis.com/youtube/v3/search', function(data){
-    console.log(data);
+  $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=clarinet&key=AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts', function(data) {
+    var thumbnails = [];
+    for (i = 0; i < data.items.length; i++){
+      thumbnails.push(data.items[i].snippet.thumbnails.default.url);    
+    }
   });
   
-//  $('#search-form').submit(function(evt) {
-//    evt.preventDefault();
-//    var searchTerm = $('#search-form__input').val();
-//  })
+  
+  
+  // $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=clarinet&key=AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts') function(data){
+//   $.each(data.items, function(index, snippet) {
+//     
+//     console.log(snippet[0]);
+//   })
+   //   var itemsArr = data.items; 
+//   var thumbnailImage = itemsArr[0].snippet.thumbnails.default.url;
+//   console.log(thumbnailImage);
+   //logResults(itemsArr);
+   //console.log(itemsArr[0].snippet.thumbnails.default);
+  
 });
 
+//function searchByKeyword() {
+//  var results = YouTube.Search.list('id, snippet', {q: 'clarinet', max: 25});
+//  for (var i in results.item) {
+//    var item = results.item[i];
+//    console.log('[%s] Title: %s', item.id.videoId, item.snippet.title)
+//  }
+//}
+  
+
+function searchResults(results) {
+  $.each(results, function(index, ))
+}
+
+function getRequest() {
+  var params = {
+    part: 'snippet',
+    key: AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts,
+    q: 'clarinet'
+  };
+  url = 'https://www.googleapis.com/youtube/v3/search';
+  $.getJSON(url, params, function(data) {
+    searchResults(data.items);  
+  });
+}
 
 
 
