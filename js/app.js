0438523a85b00039f2b430fa6d705f4a6112768c
aberkow <berkowitz.clarinet@gmail.com>
2016-03-08 20:14:12 -0500

    starting from scratch(ish)

--- js/app.js
+++ js/app.js
@@ -10,15 +10,52 @@ Bonus
 */
 
 $(document).ready(function(){
-  $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=clarinet&key=AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts', function(data) {
-    var thumbnails = [];
-    for (i = 0; i < data.items.length; i++){
-      thumbnails.push(data.items[i].snippet.thumbnails.default.url);    
-    }
+  $('#search-form').submit(function(evt) {
+    var searchTerm = $('#search-form__input').val();
+    evt.preventDefault();
+    console.log(searchTerm);
+    getRequest(searchTerm);
   });
+});
+
+// getRequest input = searchTerm, output = searchResults
+function getRequest(searchTerm){
+  var params = {
+    part: 'snippet',
+    key: 'AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts',
+    q: searchTerm
+  };
+  url = 'https://www.googleapis.com/youtube/v3/search';
+  $.getJSON(url, params, function(data){//NB - url needs to go before params
+    console.log(data.items);
+  });
+}
+
+
+
+//function getRequest(searchTerm) {
+//    var params = {
+//    part: 'snippet',
+//    key: 'AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts',
+//    q: searchTerm
+//  };
+//  url = 'https://www.googleapis.com/youtube/v3/search';
+//  $.getJSON(params, url, function(data) {
+//    console.log(data.items);
+//  });
+//}
+
+function showResults(results) {
   
-  
-  
+}
+
+//  $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=clarinet&key=AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts', function(data) {
+//    var thumbnails = [];
+//    for (i = 0; i < data.items.length; i++){
+//      thumbnails.push(data.items[i].snippet.thumbnails.default.url);    
+//    }
+//  });  
+
   // $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&q=clarinet&key=AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts') function(data){
 //   $.each(data.items, function(index, snippet) {
 //     
@@ -29,10 +66,8 @@ $(document).ready(function(){
 //   console.log(thumbnailImage);
    //logResults(itemsArr);
    //console.log(itemsArr[0].snippet.thumbnails.default);
-  
-});
 
-//function searchByKeyword() {
+  //function searchByKeyword() {
 //  var results = YouTube.Search.list('id, snippet', {q: 'clarinet', max: 25});
 //  for (var i in results.item) {
 //    var item = results.item[i];
@@ -40,25 +75,6 @@ $(document).ready(function(){
 //  }
 //}
   
-
-function searchResults(results) {
-  $.each(results, function(index, ))
-}
-
-function getRequest() {
-  var params = {
-    part: 'snippet',
-    key: AIzaSyB39NC87BtguKgxPSOUecUOtXzT_8Z88Ts,
-    q: 'clarinet'
-  };
-  url = 'https://www.googleapis.com/youtube/v3/search';
-  $.getJSON(url, params, function(data) {
-    searchResults(data.items);  
-  });
-}
-
-
-
 //function getRequst(searchTerm) {
 //  var params = {
 //    part: 'snippet',
