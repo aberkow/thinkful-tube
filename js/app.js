d5610fdef5018eb6ce3622f6cc21e2f2591ebeee
aberkow <berkowitz.clarinet@gmail.com>
2016-04-06 21:31:29 -0400

    changed to a video object

--- js/app.js
+++ js/app.js
@@ -45,21 +45,33 @@ function getRequest(searchTerm){
 
 //showResults input = results(aka data.items), output = console.log(imageurls)
 function showResults(results) {
-  
   //perhaps try setting this up as an object...?
   // var video = {html: "", image: "", etc}...
   
-  var html = "";
-  var image = ""; //the thumbnail url
-  var id = "";//the videoId to link via...
-  var link = "https://www.youtube.com/watch?v=";//this link.
+  var video = {
+    html: "",
+    image: "",
+    id: "",
+    link: "https://www.youtube.com/watch?v="
+  }
+  
+//  var html = "";
+//  var image = ""; //the thumbnail url
+//  var id = "";//the videoId to link via...
+//  var link = "https://www.youtube.com/watch?v=";//this link.
   
   for (i = 0; i < results.length; i++){
-    image = results[i].snippet.thumbnails.high.url;//display the image
-    id = results[i].id.videoId;//assign the id to link to
-    html += "<div class='results-container__item'><a href=" + link + id + " " +"target='_blank'><img src=" + image + " " + "alt='thumbnail'" + " " + "width='480' height='360' class='videoImage'></a></div>"
+    video.image = results[i].snippet.thumbnails.high.url;//display the image
+    video.id = results[i].id.videoId;//assign the id to link to
+    video.html += "<div class='results-container__item'><a href=" + video.link + video.id + " " +"target='_blank'><img src=" + video.image + " " + "alt='thumbnail'" + " " + "width='480' height='360' class='videoImage'></a></div>"
+    
+    
+    
+//    image = results[i].snippet.thumbnails.high.url;//display the image
+//    id = results[i].id.videoId;//assign the id to link to
+//    html += "<div class='results-container__item'><a href=" + link + id + " " +"target='_blank'><img src=" + image + " " + "alt='thumbnail'" + " " + "width='480' height='360' class='videoImage'></a></div>"
   }
-  $('#results-container').append(html);
+  $('#results-container').append(video.html);
 }
 
 //  $.each(results, function(index, value){
